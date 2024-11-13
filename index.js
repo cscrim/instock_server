e30185977e973c3a3033c4f32419bf2f0ebed3e3
Marwa Mohamed <marwa.m7d@gmail.com>
2024-11-13 13:09:22 -0500

    Apply migrations and run seeds for feature-S24DN-6

--- index.js
+++ index.js
@@ -4,10 +4,12 @@ import cors from "cors";
 
 const app = express();
 app.use(cors());
-
 const PORT = process.env.PORT || 8080;
 
 app.use(express.json());
+
+
+
 
 app.listen(PORT, () => {
   console.log(`running at http://localhost:${PORT}`);
